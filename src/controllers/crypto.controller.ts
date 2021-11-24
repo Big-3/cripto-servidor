d3d6d25a25fc0b2c969eb62f0bffeb9606e3e682
SWAGlot <sr.swalot@gmail.com>
2021-11-24 21:35:22 +0100

    Signtura i validació incorrectes

--- src/controllers/crypto.controller.ts
+++ src/controllers/crypto.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response} from 'express';
 import * as bic from 'bigint-conversion';
 import {KeyManagerImpl} from '../models/keyManagerImpl';
 import {generateKeys, PublicKey, PrivateKey} from '@criptografia/modul-rsa';
+import * as crypto from 'crypto';
 
 export async function generateBothKeys(req: Request, res: Response): Promise<Response>{
 	const n_bits = req.query.nbits; // https://<ip>/api/generateKeys?nbits=512
@@ -48,12 +49,18 @@ export async function encryptMessage(req: Request, res: Response): Promise<Respo
 		"c": "<encmsg>"
 	}
 	*/
-	return res.status(200).json({encmsg: encmsg});
+
+	//Procés de signatura del missatge hash
+	const hash: bigint = await bic.base64ToBigint(crypto.createHash('sha1').update(msg).digest('base64'));
+	const sign = await bic.bigintToBase64(privKey.sign(hash));
+
+	return res.status(200).json({encmsg: encmsg, sign: sign});
+
 }
 
 
 export async function decryptMessage(req: Request, res: Response): Promise<Response>{
-	const {identifier, encmsg} = req.body;
+	const {identifier, encmsg, vald} = req.body;
 	let keyManager = KeyManagerImpl.getInstance();
 
 	const privKey: PrivateKey = await keyManager.getPrivateKey(identifier);
@@ -61,5 +68,10 @@ export async function decryptMessage(req: Request, res: Response): Promise<Respo
 
 	const msg = await bic.bigintToText(privKey.decrypt(c));
 
-	return res.status(200).json({msg: msg});
+	//Procés de validació de la signatura
+	const pubKey: PublicKey = privKey.getPublicKey();
+	const validation = await bic.bigintToBase64(pubKey.verify(bic.base64ToBigint(vald)));
+
+	return res.status(200).json({msg: msg, validation: validation});
 }
+
